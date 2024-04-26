8ca2d446ddcfb7c4e2d6d78c47a92afb3ba85c5e
CormacKinsella <27350062+CormacKinsella@users.noreply.github.com>
2024-04-26 09:15:28 +0200

    add confluence links to logbook assets

--- docs/confluence/publish.sh
+++ docs/confluence/publish.sh
@@ -1,6 +1,18 @@
 # Make symlinks to files for publishing
 
+# Logbook
+
 ln -s -f ../logbook/logbook.qmd
+
+# Logbook images
+
+mkdir -p ./_site/assets
+cd ./_site/assets
+for i in ../../../logbook/assets/*.??g; do ln -s -f $i; done
+cd ../..
+
+# Meetings and schedule
+
 ln -s -f ../meetings/schedule.qmd
 cd minutes
 for i in ../../meetings/????????-*.qmd; do ln -s -f $i; done
