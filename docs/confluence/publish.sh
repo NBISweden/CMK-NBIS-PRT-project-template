64e4ce6dc7ab8ab3e75eaecbd951545593f57bc3
CormacKinsella <27350062+CormacKinsella@users.noreply.github.com>
2024-06-03 16:27:59 +0200

    imporve confluence publish script

--- docs/confluence/publish.sh
+++ docs/confluence/publish.sh
@@ -4,17 +4,21 @@
 
 ln -s -f ../logbook/logbook.qmd
 
-# Logbook images
+# Logbook and slide images
 
 mkdir -p ./_site/assets
 cd ./_site/assets
 for i in ../../../logbook/assets/*.??g; do ln -s -f $i; done
+for i in ../../../slides/assets/*.??g; do ln -s -f $i; done
 cd ../..
 
 # Meetings and schedule
 
 ln -s -f ../meetings/schedule.qmd
 cd minutes
+# Clean up prior links
+rm ????????-*.qmd 2> /dev/null
+# Add fresh links
 for i in ../../meetings/????????-*.qmd; do ln -s -f $i; done
 cd ..
 
